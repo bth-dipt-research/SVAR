af37aa66c53c473145b905aa4caace860b15e570
Michael Unterkalmsteiner <mun@bth.se>
2025-01-08 16:18:18 +0100

    Training final model for first three dimensions

--- agreementclassifier/train_agreement.py
+++ agreementclassifier/train_agreement.py
@@ -1,80 +1,109 @@
-"""
-NOTE: this implementated is outdated and replaced by nested cross-validation.
-
-We train the classifier on the agreement data that we collected in pilot 1 and 2. In total, we classified 72 requirements and the agreement statistics are stored in data/all_agree_statistics.csv.
-
-We create a classifier for each dimension. The dataset is split into training (80%), validation (10%) and test data (10%). The test data will not be used for training nor parameter optimization.
-"""
-
-
-from optuna import Trial
-from typing import Dict, Union, Any
+#from typing import Dict, Union, Any
 from setfit import TrainingArguments, SetFitModel, Trainer
 from datasets import load_dataset, DatasetDict
 from pathlib import Path
 from datetime import datetime
 import torch
 
-def model_init(params: Dict[str, Any]) -> SetFitModel:
-    params = params or {}
-    max_iter = params.get("max_iter", 100)
-    solver = params.get("solver", "liblinear")
-    params = {
-        "head_params": {
-            "max_iter": max_iter,
-            "solver": solver,
-        }
-    }
-    return SetFitModel.from_pretrained("KBLab/sentence-bert-swedish-cased", **params)
-
-def hp_space(trial: Trial) -> Dict[str, Union[float, int, str]]:
-    return {
-        "body_learning_rate": trial.suggest_float("body_learning_rate", 1e-6, 1e-3, log=True),
-        "num_epochs": trial.suggest_int("num_epochs", 1, 5),
-        "batch_size": trial.suggest_categorical("batch_size", [8, 16]),
-        "seed": trial.suggest_int("seed", 1, 40),
-        "max_iter": trial.suggest_int("max_iter", 50, 300),
-        "solver": trial.suggest_categorical("solver", ["newton-cg", "lbfgs", "liblinear"]),
-    }
+# -------------------------------------
+# Instructions
+#
+# 1. Identify with nvidia-smi which GPUs are available
+# 2. Limit, if needed, to run only on a specific GPU, e.g. with index 0,
+#    by invoking CUDA_VISIBLE_DEVICES=0 python train_agreement.py
+# -------------------------------------
 
 current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
 run_dir = "../models/agreement" / Path(current_time)
 
 dataset = load_dataset("csv", data_files="../data/all_agree_statistics.csv")
 
+best_parameters = {}
 
-dimensions = ["target", "nature", "interpretability", "reference"]
+# Best fold: 7
+# Accuracy: 0.8571428571428571
+# Nested CV average accuracy: 0.6321428571428571
+# Nested CV std deviation: 0.14965947742683544
+best_parameters["target"] = {
+    "body_learning_rate": 1.3229142620339052e-06,
+    "num_epochs": 4,
+    "batch_size": 8,
+    "seed": 31,
+    "max_iter": 57,
+    "solver": "liblinear"
+}
 
-for dim in dimensions:
+# Best fold: 4
+# Accuracy: 0.8571428571428571
+# Nested CV average accuracy: 0.7357142857142857
+# Nested CV std deviation: 0.0769309258162072
+best_parameters["nature"] = {
+    "body_learning_rate": 0.000955332145145954,
+    "num_epochs": 2,
+    "batch_size": 16,
+    "seed": 34,
+    "max_iter": 155,
+    "solver": "liblinear"
+}
+
+# Best fold: 7
+# Accuracy: 0.8571428571428571
+# Nested CV average accuracy: 0.6678571428571428
+# Nested CV std deviation: 0.08718968296952645
+best_parameters["interpretability"] = {
+    "body_learning_rate": 0.0006596212557964204,
+    "num_epochs": 2,
+    "batch_size": 16,
+    "seed": 2,
+    "max_iter": 215,
+    "solver": "liblinear"
+}
+
+#best_parameters["reference"] = {
+#    "body_learning_rate": 0.0,
+#    "num_epochs": 0,
+#    "batch_size": 0,
+#    "seed": 0,
+#    "max_iter": 0,
+#    "solver": ""
+#}
+
+
+for key, value in best_parameters.items():
+    dim = key
+    params = value
+
     print(f'Training for dimension: {dim}')
 
     dataset = dataset.class_encode_column(dim)
 
-    train_testvalid = dataset['train'].train_test_split(test_size=0.2, stratify_by_column=dim)
-    test_valid = train_testvalid['test'].train_test_split(test_size=0.5, stratify_by_column=dim)
-    ttv_dataset = DatasetDict({
-        'train': train_testvalid['train'],
-        'test': test_valid['test'],
-        'valid': test_valid['train']})
+    max_iter = params.get("max_iter", 100)
+    solver = params.get("solver", "liblinear")
+    model_params = {
+        "head_params": {
+            "max_iter": max_iter,
+            "solver": solver,
+        }
+    }
+
+    print(f'Model parameters: {model_params}')
+
+    model = SetFitModel.from_pretrained("KBLab/sentence-bert-swedish-cased", **model_params)
 
     trainer = Trainer(
-        model_init=model_init,
-        train_dataset=ttv_dataset["train"],
-        eval_dataset=ttv_dataset["valid"],
-        column_mapping={
+        model = model,
+        train_dataset = dataset["train"],
+        column_mapping = {
             "text": "text",
             dim: "label"
         }
     )
 
+    arguments = TrainingArguments.from_dict(params, ignore_extra=True)
 
-    best_run = trainer.hyperparameter_search(direction="maximize", hp_space=hp_space, n_trials=200)
-    print(best_run)
+    print(f'Training arguments: {arguments}')
 
-    trainer.apply_hyperparameters(best_run.hyperparameters, final_model=True)
-    trainer.train()
-    trainer.evaluate()
-    trainer.evaluate(ttv_dataset['test'])
+    trainer.train(arguments)
 
     model_path = run_dir / dim
     trainer.model.save_pretrained(model_path)
